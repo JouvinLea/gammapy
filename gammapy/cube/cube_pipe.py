f4c097829815f75f1ae3af8af6cd711da49d5605
Lea Jouvin <lea.jouvin@gmail.com>
2016-11-15 13:37:54 +0100

    continue implementing

--- gammapy/cube/cube_pipe.py
+++ gammapy/cube/cube_pipe.py
@@ -1,5 +1,6 @@
 # Licensed under a 3-clause BSD style license - see LICENSE.rst
-from __future__ import absolute_import, division, print_function, unicode_literals
+from __future__ import absolute_import, division, print_function, \
+    unicode_literals
 import logging
 import numpy as np
 from astropy.units import Quantity
@@ -10,6 +11,7 @@ from ..stats import significance
 from ..background import fill_acceptance_image
 from ..image import SkyImage, SkyImageList, disk_correlate
 from ..cube import SkyCube
+from .exposure import exposure_cube
 
 __all__ = ['SingleObsCubeMaker', 'StackedObsCubeMaker']
 
@@ -32,8 +34,10 @@ class SingleObsCubeMaker(object):
     ----------
     obs : `~gammapy.data.DataStoreObservation`
         Observation data
-    empty_cube : `~gammapy.cube.SkyCube`
-        Reference Cube
+    empty_cube_images : `~gammapy.cube.SkyCube`
+        Reference Cube for images in reco energy
+    empty_exposure_cube : `~gammapy.cube.SkyCube`
+        Reference Cube for exposure in true energy
     offset_band : `astropy.coordinates.Angle`
         Offset band selection
     exclusion_mask : `~gammapy.image.SkyMask`
@@ -42,26 +46,28 @@ class SingleObsCubeMaker(object):
         True if you want to save the normalisation of the bkg computed outside the exlusion region in a Table
     """
 
-    def __init__(self, obs, empty_cube, offset_band, exclusion_mask=None, save_bkg_scale=True):
+    def __init__(self, obs, empty_cube_images, empty_exposure_cube,
+                 offset_band, exclusion_mask=None, save_bkg_scale=True):
         # Select the events in the given energy and offset range
-        self.energy_bins = empty_cube.energy_axis.energy
+        self.energy_reco_bins = empty_cube_images.energy_axis.energy
         self.offset_band = offset_band
-        self.counts_cube = SkyCube.empty_like(empty_cube)
-        self.bkg_cube = SkyCube.empty_like(empty_cube)
-        self.exposure_cube = SkyCube.empty_like(empty_cube)
-        self.significance_cube = SkyCube.empty_like(empty_cube)
-        self.excess_cube = SkyCube.empty_like(empty_cube)
+        self.counts_cube = SkyCube.empty_like(empty_cube_images)
+        self.bkg_cube = SkyCube.empty_like(empty_cube_images)
+        self.significance_cube = SkyCube.empty_like(empty_cube_images)
+        self.excess_cube = SkyCube.empty_like(empty_cube_images)
+        self.exposure_cube = SkyCube.empty_like(empty_exposure_cube)
 
         self.obs_id = obs.obs_id
         events = obs.events
-        #events = events.select_energy(self.energy_band)
+        # events = events.select_energy(self.energy_band)
         self.events = events.select_offset(self.offset_band)
 
-        #self.images = SkyImageList()
-        #self.empty_image = empty_image
+        # self.images = SkyImageList()
+        # self.empty_image = empty_image
         self.header = self.empty_cube.wcs.to_header()
         if exclusion_mask:
-            self.cube_exclusion_mask = np.tile(exclusion_mask, (len(energy_bins)-1, 1, 1))
+            self.cube_exclusion_mask = np.tile(exclusion_mask,
+                                               (len(energy_bins) - 1, 1, 1))
 
         self.aeff = obs.aeff
         self.edisp = obs.edisp
@@ -73,11 +79,11 @@ class SingleObsCubeMaker(object):
         if self.save_bkg_scale:
             self.table_bkg_scale = Table(names=["OBS_ID", "bkg_scale"])
 
-    def counts_image(self):
+    def counts_cube(self):
         """Fill the counts image for the events of one observation."""
         self.counts_cube.fill_events(self.events)
 
-    def bkg_image(self, bkg_norm=True):
+    def bkg_cube(self, bkg_norm=True):
         """
         Make the background image for one observation from a bkg model.
 
@@ -87,100 +93,29 @@ class SingleObsCubeMaker(object):
             If true, apply the scaling factor from the number of counts
             outside the exclusion region to the bkg image
         """
-        for i_E in len(self.energy_bins):
-            energy_band = Energy([energy_bins[i_E].value, energy_bins[i_E + 1].value], energy_bins.unit)
-            table = self.bkg.acceptance_curve_in_energy_band(energy_band=energy_band)
+        for i_E in len(self.energy_reco_bins):
+            energy_band = Energy(
+                [energy_bins[i_E].value, energy_bins[i_E + 1].value],
+                energy_bins.unit)
+            table = self.bkg.acceptance_curve_in_energy_band(
+                energy_band=energy_band)
             center = self.obs_center.galactic
-            bkg_hdu = fill_acceptance_image(self.header, center, table["offset"], table["Acceptance"], self.offset_band[1])
-            bkg_image = Quantity(bkg_hdu.data, table["Acceptance"].unit) * bkg_image.solid_angle() * self.livetime
-            self.bkg_cube.data[i_E,:,:]= bkg_image.decompose()
-            self.bkg_cube.data[i_E,:,:]=self.counts_cube.data[i_E,:,:].value
+            bkg_hdu = fill_acceptance_image(self.header, center,
+                                            table["offset"],
+                                            table["Acceptance"],
+                                            self.offset_band[1])
+            bkg_image = Quantity(bkg_hdu.data, table[
+                "Acceptance"].unit) * bkg_image.solid_angle() * self.livetime
+            self.bkg_cube.data[i_E, :, :] = bkg_image.decompose()
+            self.bkg_cube.data[i_E, :, :] = self.counts_cube.data[i_E, :,
+                                            :].value
 
         if bkg_norm:
-            scale = self.background_norm_factor(self.counts_cube, self.bkg_cube)
+            scale = self.background_norm_factor(self.counts_cube,
+                                                self.bkg_cube)
             self.bkg_cube.data = scale * self.bkg_cube.data
             if self.save_bkg_scale:
                 self.table_bkg_scale.add_row([self.obs_id, scale])
-
-
-    def make_1d_expected_counts(self, spectral_index=2.3, for_integral_flux=False):
-        """Compute the 1D exposure table for one observation for an offset table.
-
-        Parameters
-        ----------
-        spectral_index : float
-            Assumed power-law spectral index
-        for_integral_flux : bool
-            True if you want that the total excess / exposure gives the integrated flux
-
-        Returns
-        -------
-        table : `astropy.table.QTable`
-            Two columns: offset in the FOV "theta" and expected counts "npred"
-        """
-        energy = EnergyBounds.equal_log_spacing(self.energy_band[0].value, self.energy_band[1].value, 100,
-                                                self.energy_band.unit)
-        energy_band = energy.bands
-        energy_bin = energy.log_centers
-        eref = EnergyBounds(self.energy_band).log_centers
-        spectrum = (energy_bin / eref) ** (-spectral_index)
-        offset = Angle(np.linspace(self.offset_band[0].value, self.offset_band[1].value, 10), self.offset_band.unit)
-        arf = self.aeff.evaluate(offset=offset, energy=energy_bin).T
-        npred = np.sum(arf * spectrum * energy_band, axis=1)
-        npred *= self.livetime
-
-        if for_integral_flux:
-            norm = np.sum(spectrum * energy_band)
-            npred /= norm
-
-        table = QTable()
-        table['theta'] = offset
-        table['npred'] = npred
-
-        return table
-
-    def exposure_image(self, spectral_index=2.3, for_integral_flux=False):
-        r"""Compute the exposure image for one observation.
-
-        Excess/exposure will give the differential flux at the energy Eref at the middle of the ``self.energy_band``
-
-        If ``for_integral_flux`` is true, it will give the integrated flux over the ``self.energy_band``
-
-        Exposure is define as follow:
-
-        .. math ::
-
-            EXPOSURE = \int_{E_1}^{E_2} A(E) \phi(E) * T \, dE
-
-        with ``T`` the observation livetime, ``A(E)`` the effective area,
-        the energy integration range :math:`[E_1,E_2]` given by ``self.energy_range``
-        and assuming a power law for the flux :math:`\phi(E) = \phi_{Eref} \times \frac{E}{E_{ref}}^{\gamma}`
-        with :math:`\gamma` the spectral index of the assumed power law.
-
-        If ``for_integral_flux`` is true,
-        :math:`EXPOSURE = \int_{E_1}^{E_2} A(E) \phi_{E} * T \, dE / \int \phi_{E} \, dE`
-
-        Parameters
-        ----------
-        spectral_index : float
-            Assumed power-law spectral index
-        for_integral_flux : bool
-            True if you want that the total excess / exposure gives the integrated flux
-        """
-        from scipy.interpolate import interp1d
-        # TODO: should be re-implemented using the exposure_cube function
-        table = self.make_1d_expected_counts(spectral_index, for_integral_flux)
-        exposure = SkyImage.empty_like(self.empty_image, unit=table["npred"].unit)
-
-        # calculate pixel offset from center (in world coordinates)
-        coord = exposure.coordinates()
-        offset = coord.separation(self.obs_center)
-
-        # Interpolate for the offset of each pixel
-        f = interp1d(table["theta"], table["npred"], bounds_error=False, fill_value=0)
-        exposure.data = f(offset)
-        exposure.data[offset >= self.offset_band[1]] = 0
-        self.images["exposure"] = exposure
 
     def background_norm_factor(self, counts, bkg):
         """Determine the scaling factor to apply to the background image.
@@ -199,13 +134,24 @@ class SingleObsCubeMaker(object):
         scale : float
             scaling factor between the counts and the bkg images outside the exclusion region.
         """
-        counts_sum = np.sum(self.counts_cube.data * self.cube_exclusion_mask.data)
+        counts_sum = np.sum(
+            self.counts_cube.data * self.cube_exclusion_mask.data)
         bkg_sum = np.sum(self.bkg_cube.data * self.cube_exclusion_mask.data)
         scale = counts_sum / bkg_sum
 
         return scale
 
-    def significance_image(self, radius):
+    def make_exposure_cube(self):
+        """
+        Compute the exposure cube
+        """
+        self.exposure_cube = exposure_cube(pointing=self.obs_center,
+                                           livetime=self.livetime,
+                                           aeff2d=self.aeff,
+                                           ref_cube=self.exposure_cube,
+                                           offset_max=self.offset_band[1])
+
+    def significance_cube(self, radius):
         """Make the significance image from the counts and bkg images.
 
         Parameters
@@ -213,17 +159,16 @@ class SingleObsCubeMaker(object):
         radius : float
             Disk radius in pixels.
         """
-        for i_E in len(self.energy_bins):
-            counts = disk_correlate(self.counts_cube[i_E,:,:], radius)
-            bkg = disk_correlate(self.bkg_cube[i_E,:,:], radius)
-            self.significance_cube.data[i_E,:,:] = significance(counts, bkg)
+        for i_E in len(self.energy_reco_bins):
+            counts = disk_correlate(self.counts_cube[i_E, :, :], radius)
+            bkg = disk_correlate(self.bkg_cube[i_E, :, :], radius)
+            self.significance_cube.data[i_E, :, :] = significance(counts, bkg)
 
-
-    def excess_image(self):
+    def excess_cube(self):
         """Compute excess between counts and bkg image."""
-        for i_E in len(self.energy_bins):
-        self.excess_cube.data[i_E,:,:] =  self.counts_cube[i_E,:,:] - self.bkg_cube[i_E,:,:]
-
+        for i_E in len(self.energy_reco_bins):
+            self.excess_cube.data[i_E, :, :] = self.counts_cube[i_E, :,
+                                               :] - self.bkg_cube[i_E, :, :]
 
 
 class StackedObsCubeMaker(object):
@@ -240,10 +185,10 @@ class StackedObsCubeMaker(object):
 
     Parameters
     ----------
-    empty_image : `~gammapy.image.SkyImage`
-        Reference image
-    energy_band : `~gammapy.utils.energy.Energy`
-        Energy band selection
+    empty_cube_images : `~gammapy.cube.SkyCube`
+        Reference Cube for images in reco energy
+    empty_exposure_cube : `~gammapy.cube.SkyCube`
+        Reference Cube for exposure in true energy
     offset_band : `astropy.coordinates.Angle`
         Offset band selection
     data_store : `~gammapy.data.DataStore`
@@ -252,32 +197,38 @@ class StackedObsCubeMaker(object):
         Required columns: OBS_ID
     exclusion_mask : `~gammapy.image.SkyMask`
         Exclusion mask
-    ncounts_min : int
-        Minimum counts required for the observation
     save_bkg_scale: bool
         True if you want to save the normalisation of the bkg for each run in a `Table` table_bkg_norm with two columns:
          "OBS_ID" and "bkg_scale"
     """
 
-    def __init__(self, empty_image=None, energy_band=None, offset_band=None,
-                 data_store=None, obs_table=None, exclusion_mask=None, ncounts_min=0, save_bkg_scale=True):
+    def __init__(self, empty_cube_images, empty_exposure_cube=None,
+                 offset_band=None,
+                 data_store=None, obs_table=None, exclusion_mask=None,
+                 ncounts_min=0, save_bkg_scale=True):
 
-        self.images = SkyImageList()
+        self.empty_cube_images = empty_cube_images
+        self.energy_reco_bins = empty_cube_images.energy_axis.energy
+        if not empty_exposure_cube:
+            self.empty_exposure_cube = SkyCube.empty_like(empty_cube_images)
+        else:
+            self.empty_exposure_cube = empty_exposure_cube
+
+        self.counts_cube = SkyCube.empty_like(empty_cube_images)
+        self.bkg_cube = SkyCube.empty_like(empty_cube_images)
+        self.significance_cube = SkyCube.empty_like(empty_cube_images)
+        self.excess_cube = SkyCube.empty_like(empty_cube_images)
+        self.exposure_cube = SkyCube.empty_like(empty_exposure_cube)
 
         self.data_store = data_store
         self.obs_table = obs_table
-        self.energy_band = energy_band
         self.offset_band = offset_band
 
-        self.empty_image = empty_image
-        self.header = self.empty_image.to_image_hdu().header
+        self.header = self.empty_cube.wcs.to_header()
         self.exclusion_mask = exclusion_mask
         if exclusion_mask:
-            exclusion_mask.name = 'exclusion'
-            self.images['exclusion'] = exclusion_mask
-        self.ncounts_min = ncounts_min
-        self.psfmeantab = None
-        self.thetapsf = None
+            self.exclusion_mask = exclusion_mask
+
         self.save_bkg_scale = save_bkg_scale
         if self.save_bkg_scale:
             self.table_bkg_scale = Table(names=["OBS_ID", "bkg_scale"])
@@ -300,35 +251,29 @@ class StackedObsCubeMaker(object):
             Disk radius in pixels for the significance image
         """
 
-        total_counts = SkyImage.empty_like(self.empty_image, name='counts')
-        if make_background_image:
-            total_bkg = SkyImage.empty_like(self.empty_image, name='bkg')
-            total_exposure = SkyImage.empty_like(self.empty_image, name='exposure')
-
         for obs_id in self.obs_table['OBS_ID']:
             obs = self.data_store.obs(obs_id)
-            obs_image = SingleObsCubeMaker(obs, self.empty_image, self.energy_band, self.offset_band,
-                                            self.exclusion_mask, self.ncounts_min)
-            if len(obs_image.events) <= self.ncounts_min:
-                continue
-            else:
-                obs_image.counts_image()
-                total_counts.data += obs_image.images['counts'].data
-                if make_background_image:
-                    obs_image.bkg_image(bkg_norm)
-                    if self.save_bkg_scale:
-                        self.table_bkg_scale.add_row(obs_image.table_bkg_scale[0])
-                    obs_image.exposure_image(spectral_index, for_integral_flux)
-                    total_bkg.data += obs_image.images['bkg'].data
-                    total_exposure.data += obs_image.images['exposure'].data
+            cube_images = SingleObsCubeMaker(obs,
+                                             empty_cube_images=self.empty_cube_images,
+                                             empty_exposure_cube=self.empty_exposure_cube,
+                                             offset_band=self.offset_band,
+                                             exclusion_mask=self.exclusion_mask,
+                                             save_bkg_scale=self.save_bkg_scale)
 
-        self.images['counts'] = total_counts
+            cube_images.counts_cube()
+            self.counts_cube.data += cube_images.counts_cube.data
+            if make_background_image:
+                cube_images.bkg_cube(bkg_norm)
+                if self.save_bkg_scale:
+                    self.table_bkg_scale.add_row(
+                        cube_images.table_bkg_scale[0])
+                cube_images.make_exposure_cube()
+                self.bkg_cube.data += cube_images.bkg_cube.data
+                self.exposure_cube.data += cube_images.exposure_cube.data
 
         if make_background_image:
-            self.images['bkg'] = total_bkg
-            self.images['exposure'] = total_exposure
-            self.significance_image(radius)
-            self.excess_image()
+            self.significance_cube(radius)
+            self.excess_cube()
 
     def significance_image(self, radius):
         """Make the significance image from the counts and bkg images.
@@ -338,15 +283,13 @@ class StackedObsCubeMaker(object):
         radius : float
             Disk radius in pixels.
         """
-        image = SkyImage.empty_like(self.empty_image, name='significance')
-        counts = disk_correlate(self.images['counts'].data, radius)
-        bkg = disk_correlate(self.images['bkg'].data, radius)
-        image.data = significance(counts, bkg)
-
-        self.images['significance'] = image
+        for i_E in len(self.energy_reco_bins):
+            counts = disk_correlate(self.counts_cube[i_E, :, :], radius)
+            bkg = disk_correlate(self.bkg_cube[i_E, :, :], radius)
+            self.significance_cube.data[i_E, :, :] = significance(counts, bkg)
 
     def excess_image(self):
         """Compute excess between counts and bkg image."""
-        total_excess = SkyImage.empty_like(self.empty_image, name='excess')
-        total_excess.data = self.images['counts'].data - self.images['bkg'].data
-        self.images['excess'] = total_excess
+        for i_E in len(self.energy_reco_bins):
+            self.excess_cube.data[i_E, :, :] = self.counts_cube[i_E, :,
+                                               :] - self.bkg_cube[i_E, :, :]
